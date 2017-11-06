0fd82dc32bbe6e71c91010d800426e311960acee
tonikus22 <kazakov.anton97@gmail.com>
2017-11-06 16:16:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function Pr() {
+	var a = 0;
+	var b = 0;
+	var arr = [];
+	arr[0] = function (newA, newB) {
+		if ((newA > 0) && (typeof newA === Number) && (newB > 0) && (typeof newB === Number)) {
+			a = newA;
+		    b = newB; 
+		} //else console.log('error');
+	};
+	arr[1] = function () {
+		return ((a + b) / 2);
+	};
+	arr[2] = function () {
+		return (a * b);
+	};
+	return arr;
+}
+var v = Pr();
+v[0] (4, 8);
+console.log (v[1] () ); 
+v[0] (2, 'o');
+console.log (v[2] () );
